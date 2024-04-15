dadd9200fb9cdfe53d0e570d6dac0ce2839debab
EvgenyPankov <pankovrec@yandex.ru>
2024-04-15 15:31:47 +0300

    1st web

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"log"
+	"path/filepath"
+)
+
+const (
+	webDir string = "./web"
+)
+
+func main() {
+	startWeb()
+}
+
+func startWeb() {
+http.Handle("/", http.FileServer(http.Dir(webDir)))
+
+err := http.ListenAndServe(":7540", nil)
+if err != nil {
+	panic(err)
+}
+}
